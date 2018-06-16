0d056de36a2e5f6a4813515db3b5c7db1e67ed96
alexanbilog <alexanbilog@gmail.com>
2018-06-17 02:22:41 +0700

    Upload Index.blade

---
+++ index.blade.php
@@ -0,0 +1,337 @@
+@extends('layouts.app')
+@section('title', 'MoneyGer | Homepage')
+
+@section('content')
+
+    <!-- SHOWCASE -->
+  <section id="showcase" class="py-5 mt-0">
+    <div class="dark-overlay text-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center" style="padding-top: 80px;">
+            <h1 class="mt-4 pt-4 display-3">
+              Welcome to <span style="color:#FF4F5E">Finance Tools PH</span>.com
+            </h1>
+            <h3>Your One Stop Place For Your Financial Tools</h3>
+          </div>
+          {{-- <div class="col-md-6 col-lg-6 showcase-video" style="padding-top: 60px;">
+            <div class="pt-3">
+              <div class="video-wrapper">
+                <iframe src="https://www.youtube.com/embed/pdemGjDSRlM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div> --}}
+        </div>
+      </div>
+    </div>
+  </section>
+
+    {{-- <section id="intro" class="py-4 bg-dark text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">
+          <h1 class="">How can MoneyMind help you</h1>
+          <p class="lead">Be inspired and motivated to manage your money wisely.</p>
+        </div>
+      </div>
+    </div>
+  </section> --}}
+
+  <!--Features-->
+  {{-- <div id="features" class="py-4 mb-3">
+    <div class="container">
+      
+          <div class="row">
+            <div class="col-md-6">
+              <div class="col-md-8 mx-auto">
+              <img class="img-fluid" src="images/net-worth.png" alt="">
+              </div>
+              <h4 class="text-info text-center mt-3">Create a Personal Balance Sheet</h4> 
+          </div>
+          <div class="col-md-6">
+            <div class="col-md-8 mx-auto">
+              <img class="img-fluid" src="images/financetools.png" alt="">
+            </div>
+            <h4 class="text-info text-center mt-4">Use Our Financial Tools and Calculators</h4>
+          </div>
+          </div>
+          
+          <div class="row mt-4">
+            <div class="col-md-6">
+              <div class="col-md-8 mx-auto">
+              <img class="img-fluid" src="images/incomesaving.png" alt="">
+            </div>
+              <h4 class="text-info text-center mt-3">Manage Your Income and Spending</h4>
+            </div>
+          
+          
+            <div class="col-md-6">
+              <div class="col-md-8 mx-auto">
+                <img class="img-fluid" src="images/net-worth.png" alt="">
+              </div>
+              <h4 class="text-info text-center mt-2">Learn How to Manage and Grow your Money Wisely</h4>
+            </div>
+          </div> 
+    </div>
+  </div> --}}
+
+  <!--FINANCIAL TOOLS SECTION-->
+  <section id="calcu" class="py-4 mb-0 bg-dark">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <h3 class="display-4 text-white">Financial Tools</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  <section id="calculators">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-4 col-md-12 col-sm-12">
+              <div class="invest-cal">
+                <img src="images/calculator.jpg" alt="">
+                  <div class="content overlay text-center text-white">
+                    <h2 class="p-4 mt-4">Savings Calculator</h2>
+                    <p class="lead">Calculate how much your money grow over a period of time using the power of compound interest</p>
+                    <a href="" class="btn btn--danger text-white px-5" data-toggle="modal" data-target="#investCal">Try it Out</a>
+                  </div>
+              </div>
+          </div>
+          <!--Invest Modal-->
+                <div class="modal fade" id="investCal" tabindex="-1" role="dialog">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header bg--danger text-white">
+                        <h5 class="modal-title">Savings Calculator</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body text-center">
+                        <div class="m-4 lead">
+                          <label>Amount:</label>
+                          <input class="form-control" id="amount" type="number" name="amount" >   
+                          <label>Interest (%):</label>
+                          <input class="form-control" id="rate" type="number"   name="company" >
+                          <label>Years:</label>
+                          <input class="form-control" id="years" type="number"  name="years" >
+                        </div>
+                        <p>
+                        <a class="col-sm-4 btn btn--danger text-white mb-3" onclick="computeSavings()">Calculate</a>
+                       
+                      </p>
+                        <div class="card mx-1 mb-2">
+                            <div class="card-body text-center">
+                                <p id="title"></p>
+                                <h3 class="text-info" id="payment"></h3>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="modal-footer">
+                        <p class="text-muted text-left">This calculator is designed for educational purposes only.  The results presented by this calculator are hypothetical and may not reflect the actual growth of your own investments.</p>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+      
+          <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="week-cal">
+              <img src="images/saving.jpeg" alt="">
+                <div class="content overlay text-white text-center">
+                  <h2 class="px-3 py-4 mt-4">52-Week Calculator</h2>
+                  <p class="lead">An easy way to know how much money will you be able to save at the end of the 52 week</p><br>
+                  <a href="" class="btn btn--danger text-white px-5" data-toggle="modal" data-target="#savingsCal">Try it Out</a>
+                </div> 
+            </div> 
+        </div>
+
+        <!--52-Week Modal-->
+                <div class="modal fade" id="savingsCal" tabindex="-1" role="dialog">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header bg--danger">
+                        <h5 class="text-white">52-Week Savings Calculator</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                          <div class="text-center">
+                              <p>Choose the amount you plan to save in the first week then click calculate. You will see the total amount of money you could save after the 52nd week.</p>
+                                <label>Start Amount</label>            
+                                    <select name="" id="start-amount" class="form-control">
+                                        <option value="0">-Select-</option>
+                                        <option value="1378" id="start1">1 peso</option>
+                                        <option value="6890" id="start5">5 pesos</option>
+                                        <option value="13780" id="start10">10 pesos</option>
+                                        <option value="68900" id="start100">50 pesos</option>
+                                        <option value="137800" id="start200">100 pesos</option>
+                                    </select>
+                                      <button id="total-saving" class=" col-sm-4 btn btn--danger text-white my-3" onclick="selectAmount()" >Calculate</button>
+                            </div>
+                            <div class="card mx-1 mb-2">
+                                <div class="card-body text-center">
+                                    <p id="resdecript"></p>
+                                    <h3 class="text-info" id="week-result"></h3>
+                                </div>
+                            </div> 
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+        <div class="col-lg-4 col-md-12 col-sm-12">
+          <div class="retirement-cal">
+            <img src="images/retirement.jpg" alt="">
+              <div class="content overlay text-white text-center">
+                <h2 class="px-3 py-4 mt-4">Retirement Calculator</h2>
+                <p class="lead">Know how much you need to save now to have a comfortable retirement in the future.</p><br>
+                <a href="" class="btn btn--danger text-white px-5" data-toggle="modal" data-target="#retirementCal">Try it Out</a>
+              </div>  
+          </div>
+        </div>
+
+        <!--Retirement Modal-->
+                <div class="modal fade" id="retirementCal" tabindex="-1" role="dialog">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header bg--danger">
+                          <h5 class="text-white">Retirement Calculator</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="text-center">
+                                <p>View the potential retirement amount that you can get upon retirement based on the fund that you set aside and invest today. Your retirement fund will start to grow annually until your target retirement years based on the interest you choose. Fill in all the fields and click calculate.</p>
+                                  <div class="lead text-left">
+                                    <label class="mt-3">Plan Retirement Age</label>
+                                      <input type="number" id="retirementAge" class="form-control" placeholder="60">
+                                    <label class="mt-3">Current Age</label>
+                                      <input type="number" id="currentAge" class="form-control" placeholder="Enter your current Age">
+                                    <label class="mt-3">Invested Amount for Retirement</label>
+                                      <input type="number" id="annualCon" class="form-control" placeholder="100,000">
+                                    <label class="mt-3">Investment Rate of Return</label>            
+                                      <select name="" id="interest" class="form-control">
+                                          <option value="0">-Select-</option>
+                                          <option value="5" id="start200">5%</option>
+                                          <option value="8" id="start1">8%</option>
+                                          <option value="10" id="start5">10%</option>
+                                          <option value="12" id="start10">12%</option>
+                                          <option value="15" id="start100">15%</option>
+                                      </select>
+                                    </div>
+                                    <button id="retirementAmount" class=" col-sm-4 btn btn--danger text-white my-3" onclick="selectRetireAmount()" >Calculate</button>
+                              </div>
+                              <div class="card mx-1 mb-2">
+                                  <div class="card-body text-center">
+                                      <p class="mb-0 pb-0" id="retdescript"></p>
+                                      <h3 class="text-info" id="ret-result"></h3>
+                                      <!-- <p class="mb-0 pb-0" id="retdescript2"></p> -->
+                                  </div>
+                              </div> 
+                        </div>
+                        <div class="modal-footer">
+                          <p class="disclaimer-text text-left">NOTE: This calculator is for illustrative purposes only and does not reflect the performance of any specific investment. It does not take into effect the deduction of any fees or taxes. There is no guarantee that the rate of return selected can actually be achieved. Investments offering the potential for higher rates of return also involve a higher degree of risk.<br>
+                          <br>*Source: LIMRA, Across Generations: Retirement Savings, Attitudes and Engagement, 2015</p>
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="calculators">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-md-12 col-sm-12">
+                <div class="invest-cal">
+                  <img src="images/sss.png" alt="sss-image">
+                    <div class="content overlay text-center text-white">
+                      <h2 class="p-4 mt-4">SSS Calculator</h2>
+                      <p class="lead">Know how much pension you'll get from SSS</p>
+                      <a href="{{ url('/sss_loan_calculator') }}" class="btn btn--danger text-white px-5">Try it Out</a>
+                    </div>
+                </div>
+              </div>
+            <div class="col-lg-4 col-md-12 col-sm-12">
+              <div class="week-cal">
+                <img src="images/tax.jpg" alt="tax-image">
+                  <div class="content overlay text-white text-center">
+                    <h2 class="px-3 py-4 mt-4">Tax Calculator</h2>
+                    <p class="lead">How much are you going to pay to the goverment</p><br>
+                    <a href="{{ url('taxcalculator') }}" class="btn btn--danger text-white px-5">Try it Out</a>
+                  </div> 
+              </div> 
+          </div>
+          <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="retirement-cal">
+              <img src="images/vul.jpg" alt="">
+                <div class="content overlay text-white text-center">
+                  <h2 class="px-3 py-4 mt-4">Investment Calculator</h2>
+                  <p class="lead">See how much your money worth in the future in different investment vehicle</p><br>
+                  <a href="{{ url('investmentcalculator') }}" class="btn btn--danger text-white px-5">Try it Out</a>
+                </div>  
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- BLOG SECTION-->
+{{-- <section id="blogs" class="bg--warning py-4 my-4">
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">
+          <h3 class="display-4 text-white">Financial Inspiration Blog</h3>
+        </div>
+      </div>
+    </div>
+  </section> --}}
+
+{{-- <section id="blog">
+  <div class="container">
+    <div class="row">
+        @if(count($posts) > 0)
+        @foreach($posts as $post)
+        <div class="col-lg-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" style="height: 13rem;" src="{{ asset('images/' . $post->image) }}" alt="Card image cap">
+                <div class="card-body pt-1 px-2">
+                  <a href="/blog/{{$post->slug}}" class="nav-link pt-0 px-0"><h5 class="card-title">{{ $post->title }}</a></h5>
+                  <p class="card-text">{!! str_limit($post->description, 150) !!}</p>
+                </div>
+                <div class="card-footer">
+                    <a href="/blog/{{$post->slug}}" class="btn btn-outline-success float-right">Read More</a>
+                  </div>
+              </div>
+            </div>
+              @endforeach
+          @else 
+          <p>No Posts Found</p>
+      @endif
+    </div>
+
+      <!-- Pagination-->
+      <div class="row">
+          <div class="mx-auto">
+              {!! $posts->links('vendor.pagination.bootstrap-4'); !!}
+          </div>
+      </div>   
+        <div class="text-center">
+          <a href="{{ url('/blog') }}" class="btn btn--primary px-3 mb-4  btn-lg text-white"> More Financial Blog</a>
+        </div>
+    </div>
+  </div>
+</section> --}}
+
+@endsection
